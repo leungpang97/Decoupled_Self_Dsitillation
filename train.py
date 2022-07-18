8ca3c6afde9e000be42b507bf57f1acea2ed3d00
leungpang97 <99473112+leungpang97@users.noreply.github.com>
2022-07-18 11:13:21 +0800

    Update train.py

--- train.py
+++ train.py
@@ -276,8 +276,8 @@ if __name__ == "__main__":
             #   for shallow classifiers
             for index in range(1, len(outputs)):
                 #   logits distillation.
-                **kwargs
-                loss += min(kwargs["epoch"] / 20, 1.0) *dkd_loss(outputs[index], teacher_output,labels,1,8,args.temperature) * args.loss_coefficient
+               
+                loss += min(min(epoch,20) / 20, 1.0) *dkd_loss(outputs[index], teacher_output,labels,1,8,args.temperature) * args.loss_coefficient
                 loss += criterion(outputs[index], labels) * (1 - args.loss_coefficient)
                 
                 #   feature distillation
